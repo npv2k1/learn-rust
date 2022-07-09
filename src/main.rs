7601d4b56f3c5c5aeb1977021fbbcd9ff425b58d
npv2k1 <npv2k1@gmail.com>
2022-07-09 19:47:07 +0700

    variable

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
+fn variable(){
+  let x:i32 = 10;
+  let y:i32 = 20;
+
+  let z = x+y;
+
+//   let mut arr  = [1,2,4];
+//   let z1 = &arr;
+//   arr[0]=999;
+  println!("x+y = {}",z);
+//   println!("&z = {}",arr[0]);
+//   println!("&z = {}",z1[0]);
+}
 fn main() {
     let name = "Nguyen";
     println!("Hello, {}",name);
+    variable()    
 }
 
